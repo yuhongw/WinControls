339b24ff8a225f63f29786dddac6ed76cfec292c
GCZX <187120999@qq.com>
2017-06-27 08:17:00 +0800

    Test

---
+++ SwitchInfo/WebTests1/SiteSvcTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Web.Tests
+{
+    [TestClass()]
+    public class SiteSvcTests
+    {
+
+        const string RootPath = @"F:\Projs\cn\wincontrols\SwitchInfo\Web\Data";
+        [TestMethod()]
+        public void ReadDataTest()
+        {
+            SiteSvc svc = new SiteSvc(RootPath);
+            var data = svc.GetSample();
+            svc.SaveData(data);
+            List<Site> sites = svc.ReadData();
+            Assert.AreEqual(true,sites.Count>0);
+        }
+
+        [TestMethod()]
+        public void SaveDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetSampleTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
