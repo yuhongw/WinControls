67972022f2575fcc1b6610a7c75e29d1bd615476
Bruce Yu <yuhon@live.com>
2017-08-20 19:24:31 +0800

    call by vb6

---
+++ CallByVB6/CallByVB6/IUtility.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace CallByVB6
+{
+    [Guid("4191AEF9-BE9A-4453-90B5-E49433F46BA1")]
+    public interface IUtility
+    {
+        string StrToPinyin(string str);
+    }
+}
---
+++ CallByVB6/CallByVB6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallByVB6
+{
+    //for test
+    class Program
+    {
+        public static void Main()
+        {
+            Utility util = new Utility();
+            Console.WriteLine(util.StrToPinyin("你好,做饭.回家还是不回家？"));
+            Console.ReadKey();
+        }
+    }
+}
